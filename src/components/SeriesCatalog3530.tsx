21d26309f19b7919e747922654edea6ee8d32ac8
Aleks <aleks@example.com>
2025-07-07 05:21:52 +0300

    Ваше сообщение коммита, например: Добавил каталог коммутаторов и исправил баги

--- src/components/SeriesCatalog3530.tsx
+++ src/components/SeriesCatalog3530.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState,} from "react";
 import { Button } from "@/components/ui/button";
 import Icon from "@/components/ui/icon";
 import { motion } from "framer-motion";
